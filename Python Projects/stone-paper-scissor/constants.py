5c771b4b4960c3d455c4eb5a85bbab1f44494473
bindagearyan-bit <bindagearyan@gmail.com>
2026-08-09 21:58:02 +0530

    Create constants.py

---
+++ Python Projects/stone-paper-scissor/constants.py
@@ -0,0 +1,11 @@
+VALID_CHOICES = ["rock", "paper", "scissor"]
+
+NUM_TO_WORD = {
+    "0": "rock",
+    "1": "paper",
+    "2": "scissor"
+}
+
+WIN_MESSAGE = "You Win!"
+LOSE_MESSAGE = "You Lose!"
+DRAW_MESSAGE = "It's a draw!"
