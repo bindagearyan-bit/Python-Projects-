36f884683e7510312631c2e9cf0175a86c6ee624
bindagearyan-bit <bindagearyan@gmail.com>
2026-08-09 21:57:34 +0530

    Create rules.py

---
+++ Python Projects/stone-paper-scissor/rules.py
@@ -0,0 +1,12 @@
+def show_rules():
+    print("""
+    ROCK PAPER SCISSORS - RULES
+    ----------------------------
+    Rock beats Scissors
+    Scissors beats Paper
+    Paper beats Rock
+    Same choice = Draw
+    """)
+
+if __name__ == "__main__":
+    show_rules()
